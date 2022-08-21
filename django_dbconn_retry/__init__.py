9bd7fa919da5a1938a11d94431db5b27c115da1c
Sylvain Letourneau <sylvain.letourneau@magika.ca>
2022-08-21 07:32:24 -0400

    Django < 4.2

--- django_dbconn_retry/__init__.py
+++ django_dbconn_retry/__init__.py
@@ -7,5 +7,5 @@ from django_dbconn_retry.apps import pre_reconnect, post_reconnect, monkeypatch_
 
 __all__ = [pre_reconnect, post_reconnect, monkeypatch_django, DjangoIntegration]
 
-if django.VERSION < (3, 2):
+if django.VERSION < (4, 2):
     default_app_config = 'django_dbconn_retry.DjangoIntegration'
